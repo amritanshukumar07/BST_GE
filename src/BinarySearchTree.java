6b0cfde6301dee6a8b7738372566a933fbfc3a6e
Amritanshu Kumar <amritanshu8271@gmail.com>
2024-12-30 21:33:49 +0530

    UC1

---
+++ src/BinarySearchTree.java
@@ -0,0 +1,35 @@
+public class BinarySearchTree <T extends Comparable<T>>{
+    private MyBinaryNode<T> root;
+
+    public void add(T key){
+        root = addRecursively(root,key);
+
+    }
+
+    private MyBinaryNode<T> addRecursively(MyBinaryNode<T> current,T key){
+        if(current==null){
+            return new MyBinaryNode<>(key);
+
+
+        }
+        if(key.compareTo(current.key)<0){
+            current.left= addRecursively(current.left,key);
+        }else if(key.compareTo(current.key)>0){
+            current.right= addRecursively(current.right,key);
+        }
+        return current;
+    }
+
+    public void printInOrder(){
+        printInOrderRecursively(root);
+        System.out.println();
+    }
+
+    private void printInOrderRecursively(MyBinaryNode<T> node){
+        if(node!=null){
+            printInOrderRecursively(node.left);
+            System.out.print(node.key+ " ");
+            printInOrderRecursively(node.right);
+        }
+    }
+}
